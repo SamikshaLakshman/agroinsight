680cb8af62650b4a1e73f95768562759b2f04625
Samiksha Lakshman Swamy <samikshal2404@gmail.com>
2026-06-26 00:20:48 +0530

    Update App.jsx to new version
    as told by sgn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./context/ThemeContext"; 
 import Layout from "./components/Layout";
 import ProtectedRoute from "./components/ProtectedRoute";
 
